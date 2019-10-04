61f1f04d810483c1046acd7a65d5d3c30091f092
chris1085 <itscfchiang@gmail.com>
2019-10-04 17:25:51 +0800

    change progress status by JSON file

--- js/all.js
+++ js/all.js
@@ -1,31 +1,77 @@
 function getJSONList(path) {
-  $.getJSON(path, function (data) {
-    // console.log(data); //json output
-    for (let prorgressIndex = 0; prorgressIndex < data.length; prorgressIndex++) {
-      const runid = data[prorgressIndex].runid;
-      const sequenced = data[prorgressIndex].sequenced;
-      const analyzed = data[prorgressIndex].analyzed;
-      const onDell = data[prorgressIndex].ondell;
-      const converted = data[prorgressIndex].converted;
-      const inCloud = data[prorgressIndex].incloud;
-      const downloaded = data[prorgressIndex].downloaded;
-      const jobSubmitted = data[prorgressIndex].jobsubmitted;
-      const jobCompleted = data[prorgressIndex].jobcompleted;
-      const backToDell = data[prorgressIndex].backtodell;
 
-      if (path == '../NIPS/nips_progress_tail.json') {
-        $('#nips-runId' + prorgressIndex).text(runid);
-      } else if (path == '../SG/sg_progress_tail.json') {
-        $('#sg-runId' + prorgressIndex).text(runid);
-      } else if (path == '../IONA/iona_progress_tail.json') {
-        $('#iona-runId' + prorgressIndex).text(runid);
-      }
+  let progressJson = {};
+  $.getJSON(path, function (data) {
+    // console.log(data); //JOSN output
+    // console.log(data[0].runid);
+
+    if (path == '../NIPS/nips_progress_tail.json') {
+      data.forEach(function (sampleRun, index) {
+        // console.log(sampleRun);
+        $('#nips-runId' + index).text(sampleRun.runid);
+        // console.log(sampleRun);
+
+        if (sampleRun.closed == 0) {
+
+          console.log(sampleRun);
+
+          if (sampleRun.sequenced == 0 && sampleRun.error == 0) {
+            $("#nips-progressId" + index).find(".stepbar-progress").attr("data-current-step", "sequenced");
+            $("#nips-progressId" + index).find(".stepbar-progress").attr("data-step-status", "pending");
+
+            return;
+          }
+          if (sampleRun.error == 1) {
+            let failStep;
+            // console.log(sampleRun.converted);
+            Object.keys(sampleRun).some(function (key) {
+              if (sampleRun[key] == 0 && key != "error" && key != "closed") {
+                failStep = key;
+              }
+            });
+            $("#nips-progressId" + index).find(".stepbar-progress").attr("data-current-step", failStep);
+            $("#nips-progressId" + index).find(".stepbar-progress").attr("data-step-status", "fail");
+          } else {
+            Object.keys(sampleRun).some(function (key) {
+              if (sampleRun[key] == 0 && key != "error" && key != "closed") { //active
+                // console.log(key + '=' + sampleRun[key]);
+                $("#nips-progressId" + index).find(".stepbar-progress").attr("data-current-step", key);
+                $("#nips-progressId" + index).find(".stepbar-progress").attr("data-step-status", "active");
+              }
+            });
+          }
+        } else {
+          // console.log($("#nips-progressId" + index).find(".stepbar-progress"));
+          $("#nips-progressId" + index).find(".stepbar-progress").attr("data-current-step", "backtodell");
+          $("#nips-progressId" + index).find(".stepbar-progress").attr("data-step-status", "pass");
+        }
+
+      });
+    } else if (path == '../SG/sg_progress_tail.json') {
+      data.forEach(function (sampleRun, index) {
+        $('#sg-runId' + index).text(sampleRun.runid);
+
+        Object.keys(sampleRun).forEach(function (key) {
+          // console.log(key + '=' + sampleRun[key]);
+
+        });
+      });
+
+    } else if (path == '../IONA/iona_progress_tail.json') {
+      data.forEach(function (sampleRun, index) {
+        $('#sg-runId' + index).text(sampleRun.runid);
+        Object.keys(sampleRun).forEach(function (key) {
+          // console.log(key + '=' + sampleRun[key]);
+
+        });
+      });
     }
   });
+
+  return progressJson;
 }
 
 function selectStepID(iconInfo) {
-  // console.log(iconInfo);
 
   const productStep = {
     nips: {
@@ -44,79 +90,69 @@ function selectStepID(iconInfo) {
   return productStep[iconInfo.productType][iconInfo.iconStepId];
 }
 
-function modifyCollapseContent(sampleRunId = 0, stepId = 0) {}
-
-$(document).ready(function () {
-  var JSONListPath = [
-    '../NIPS/nips_progress_tail.json',
-    '../SG/sg_progress_tail.json',
-    '../IONA/iona_progress_tail.json',
-  ];
-
-  JSONListPath.forEach(getJSONList);
-
+function clickProgressIcon() {
   $('.step-body').click(function (event) {
-    var id = $(this).parents('div').attr('id').split('-progressId')[1];
-    var productType = $(this).parents('div').attr('id').split('-progressId')[0];
+    let id = $(this).parents('div').attr('id').split('-progressId')[1];
+    let productType = $(this).parents('div').attr('id').split('-progressId')[0];
 
     if (typeof $(this).attr('id') == 'undefined') {
       console.log('Please check id again');
     } else {
       $(this).parents().find('#collapseStepInfo' + id).children().empty();
 
-      var iconStepId = $(this).attr('id').split('icon-')[1];
-      var iconInfo = {
+      let iconStepId = $(this).attr('id').split('icon-')[1];
+      let iconInfo = {
         productType: productType,
         id: id,
         iconStepId: iconStepId,
       };
 
-      var selectedID = selectStepID(iconInfo);
-      var collapaseContent = 'Indicated Step: ' + selectedID;
-      var currentStatus = "Status: " + $(this).parents("table").attr('data-step-status');
+      let selectedID = selectStepID(iconInfo);
+      let collapaseContent = 'Indicated Step: ' + selectedID;
+      let currentStatus = "Status: " + $(this).parents("table").attr('data-step-status');
+      let sampleRunNumber = $(this).parents('.div-progress').find('.row-sampleRun').length;
 
       $(this).parents().find('#collapseStepInfo' + id).children().append("<div class='col-6 text-left'>" + collapaseContent + "</div>");
-      $(this).parents().find('#collapseStepInfo' + id).children().append("<div class='col-6 text-right'>" + currentStatus + "</div>");
-
-
-
+      $(this).parents().find('#collapseStepInfo' + id).children().append("<div class='col-6 text-right' id='contentStatus'>" + currentStatus + "</div>");
       // $(this).parents().find('#collapseStepInfo' + id).children().text(collapaseContent);
-
-      var sampleRunNumber = $(this).parents('.div-progress').find('.row-sampleRun').length;
 
       for (let i = 0; i < sampleRunNumber; i++) {
         if (id != i) {
           $(this).parents().find('#collapseStepInfo' + i).removeClass('show');
         }
       }
-      // console.log(sampleRunNumber);
 
       if (
         $(this).parents().find('#collapseStepInfo' + id).val() != '' && $(this).parents().find('#collapseStepInfo' + id).val() != $(this).attr('id')) {
 
         $(this).parents().find('#collapseStepInfo' + id).val($(this).attr('id'));
-        // $(this).parents().find('#collapseStepInfo' + id).addClass('show');
         $(this).parents().find('#collapseStepInfo' + id).removeClass('show');
 
         // console.log($(this).parents("div").attr('id') + " " + $(this).parents().find("#collapseStepInfo" + id).val());
-        // } else if ($(this).parents().find('#collapseStepInfo' + id).val() == '') {
-        //   $(this).parents().find('#collapseStepInfo' + id).val($(this).attr('id'));
-        // }
       } else {
         $(this).parents().find('#collapseStepInfo' + id).val($(this).attr('id'));
-        console.log($(this).parents().find('#collapseStepInfo' + id).val());
+        // console.log($(this).parents().find('#collapseStepInfo' + id).val());
       }
-
-
     }
 
   });
-  // $(".step-body").hover(
-  //   function () {
-  //     $(".collapse").collapse("show");
-  //   },
-  //   function () {
-  //     $(".collapse").collapse("hide");
-  //   }
-  // );
+}
+
+function getProgressContent(progressJson, productType, arr) {
+  // console.log(progressJson);
+  // console.log(arr);
+
+
+}
+
+$(document).ready(function () {
+  const JSONListPath = [
+    '../NIPS/nips_progress_tail.json',
+    '../SG/sg_progress_tail.json',
+    '../IONA/iona_progress_tail.json',
+  ];
+
+  let progressJson = JSONListPath.forEach(getJSONList);
+  clickProgressIcon();
+
 });
