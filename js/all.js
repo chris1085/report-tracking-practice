3c178bcdfb5ea0c2fe20ee01a54a7270a76eaaad
chris1085 <itscfchiang@gmail.com>
2019-10-05 17:34:25 +0800

    Modify getJSON function and add statusDetail

--- js/all.js
+++ js/all.js
@@ -1,74 +1,41 @@
 function getJSONList(path) {
 
-  let progressJson = {};
-  $.getJSON(path, function (data) {
+  let statusDetail;
+
+  // var xhr = new XMLHttpRequest();
+  // xhr.open(path, false);
+  // xhr.send(null);
+  // console.log(xhr);
+
+
+  var test = $.getJSON(path, function (data) {
     // console.log(data); //JOSN output
     // console.log(data[0].runid);
 
     if (path == '../NIPS/nips_progress_tail.json') {
-      data.forEach(function (sampleRun, index) {
-        // console.log(sampleRun);
-        $('#nips-runId' + index).text(sampleRun.runid);
-        // console.log(sampleRun);
-
-        if (sampleRun.closed == 0) {
-
-          console.log(sampleRun);
-
-          if (sampleRun.sequenced == 0 && sampleRun.error == 0) {
-            $("#nips-progressId" + index).find(".stepbar-progress").attr("data-current-step", "sequenced");
-            $("#nips-progressId" + index).find(".stepbar-progress").attr("data-step-status", "pending");
-
-            return;
-          }
-          if (sampleRun.error == 1) {
-            let failStep;
-            // console.log(sampleRun.converted);
-            Object.keys(sampleRun).some(function (key) {
-              if (sampleRun[key] == 0 && key != "error" && key != "closed") {
-                failStep = key;
-              }
-            });
-            $("#nips-progressId" + index).find(".stepbar-progress").attr("data-current-step", failStep);
-            $("#nips-progressId" + index).find(".stepbar-progress").attr("data-step-status", "fail");
-          } else {
-            Object.keys(sampleRun).some(function (key) {
-              if (sampleRun[key] == 0 && key != "error" && key != "closed") { //active
-                // console.log(key + '=' + sampleRun[key]);
-                $("#nips-progressId" + index).find(".stepbar-progress").attr("data-current-step", key);
-                $("#nips-progressId" + index).find(".stepbar-progress").attr("data-step-status", "active");
-              }
-            });
-          }
-        } else {
-          // console.log($("#nips-progressId" + index).find(".stepbar-progress"));
-          $("#nips-progressId" + index).find(".stepbar-progress").attr("data-current-step", "backtodell");
-          $("#nips-progressId" + index).find(".stepbar-progress").attr("data-step-status", "pass");
-        }
-
-      });
+      getProgressStatus(data, 0);
+      statusDetail = getStatusDetail(data, 0);
+      doit(statusDetail);
     } else if (path == '../SG/sg_progress_tail.json') {
-      data.forEach(function (sampleRun, index) {
-        $('#sg-runId' + index).text(sampleRun.runid);
-
-        Object.keys(sampleRun).forEach(function (key) {
-          // console.log(key + '=' + sampleRun[key]);
-
-        });
-      });
+      getProgressStatus(data, 1);
+      statusDetail = getStatusDetail(data, 1);
 
     } else if (path == '../IONA/iona_progress_tail.json') {
-      data.forEach(function (sampleRun, index) {
-        $('#sg-runId' + index).text(sampleRun.runid);
-        Object.keys(sampleRun).forEach(function (key) {
-          // console.log(key + '=' + sampleRun[key]);
-
-        });
-      });
+      getProgressStatus(data, 2);
+      statusDetail = getStatusDetail(data, 2);
     }
+    return statusDetail;
   });
+  // console.log(test);
+  return test;
 
-  return progressJson;
+}
+
+function doit(statusDetail) {
+  clickProgressIcon(statusDetail);
+  // console.log(statusDetail);
+
+  // return statusDetail;
 }
 
 function selectStepID(iconInfo) {
@@ -90,10 +57,13 @@ function selectStepID(iconInfo) {
   return productStep[iconInfo.productType][iconInfo.iconStepId];
 }
 
-function clickProgressIcon() {
+function clickProgressIcon(statusDetail) {
+  console.log(statusDetail);
+
   $('.step-body').click(function (event) {
     let id = $(this).parents('div').attr('id').split('-progressId')[1];
     let productType = $(this).parents('div').attr('id').split('-progressId')[0];
+
 
     if (typeof $(this).attr('id') == 'undefined') {
       console.log('Please check id again');
@@ -112,8 +82,8 @@ function clickProgressIcon() {
       let currentStatus = "Status: " + $(this).parents("table").attr('data-step-status');
       let sampleRunNumber = $(this).parents('.div-progress').find('.row-sampleRun').length;
 
-      $(this).parents().find('#collapseStepInfo' + id).children().append("<div class='col-6 text-left'>" + collapaseContent + "</div>");
-      $(this).parents().find('#collapseStepInfo' + id).children().append("<div class='col-6 text-right' id='contentStatus'>" + currentStatus + "</div>");
+      $(this).parents().find('#collapseStepInfo' + id).children().append("<div class='col-6 text-left'><h6>" + collapaseContent + "</h6></div>");
+      $(this).parents().find('#collapseStepInfo' + id).children().append("<div class='col-6 text-right' id='contentStatus'><h6>" + currentStatus + "</h6><h6> " + currentStatus + "</h6></div>");
       // $(this).parents().find('#collapseStepInfo' + id).children().text(collapaseContent);
 
       for (let i = 0; i < sampleRunNumber; i++) {
@@ -138,11 +108,102 @@ function clickProgressIcon() {
   });
 }
 
-function getProgressContent(progressJson, productType, arr) {
-  // console.log(progressJson);
-  // console.log(arr);
+function getProgressStatus(progressContent = data, productTypeIndex = 1) {
 
+  const productType = ["#nips", "#sg", "#iona"];
+  // console.log(progressContent);
+  // console.log(productTypeIndex);
+  progressContent.forEach(function (sampleRun, index) {
+    // console.log(sampleRun);
+    $(productType[productTypeIndex] + '-runId' + index).text(sampleRun.runid);
+    // console.log(sampleRun);
 
+    if (sampleRun.closed == 0) { //fail, active or pending progress info added
+
+      // console.log(sampleRun);
+
+      if (sampleRun.sequenced == 0 && sampleRun.error == 0) { //pending progress info added
+        $(productType[productTypeIndex] + "-progressId" + index).find(".stepbar-progress").attr("data-current-step", "sequenced");
+        $(productType[productTypeIndex] + "-progressId" + index).find(".stepbar-progress").attr("data-step-status", "pending");
+
+        return;
+      }
+      if (sampleRun.error == 1) { //fail progress info added
+        let failStep;
+        // console.log(sampleRun.converted);
+        Object.keys(sampleRun).some(function (key) { //loop to find progress key and value
+          if (sampleRun[key] == 0 && key != "error" && key != "closed") {
+            failStep = key;
+          }
+        });
+        $(productType[productTypeIndex] + "-progressId" + index).find(".stepbar-progress").attr("data-current-step", failStep);
+        $(productType[productTypeIndex] + "-progressId" + index).find(".stepbar-progress").attr("data-step-status", "fail");
+      } else { //active progress info added
+        Object.keys(sampleRun).some(function (key) {
+          if (sampleRun[key] == 0 && key != "error" && key != "closed") { //active
+            // console.log(key + '=' + sampleRun[key]);
+            $(productType[productTypeIndex] + "-progressId" + index).find(".stepbar-progress").attr("data-current-step", key);
+            $(productType[productTypeIndex] + "-progressId" + index).find(".stepbar-progress").attr("data-step-status", "active");
+          }
+        });
+      }
+    } else { //pass progress info added
+      // console.log($("#nips-progressId" + index).find(".stepbar-progress"));
+      $(productType[productTypeIndex] + "-progressId" + index).find(".stepbar-progress").attr("data-current-step", "backtodell");
+      $(productType[productTypeIndex] + "-progressId" + index).find(".stepbar-progress").attr("data-step-status", "pass");
+    }
+  });
+}
+
+function getStatusDetail(progressContent = data, productTypeIndex = 1) {
+  const productType = ["#nips", "#sg", "#iona"];
+  let statusDetail = [];
+
+  let regexp = /(\d+)\/(\d+)/;
+  // console.log(progressContent);
+
+  progressContent.forEach(function (sampleRun, index) {
+    let incloudNumber = 0;
+    let downloadedNumber = 0;
+    let jobsubmittedNumber = 0;
+    let jobcompletedNumber = 0;
+
+    // console.log(sampleRun);
+
+    if (sampleRun.incloud.match(regexp) && sampleRun.incloud.match(regexp)[1] == sampleRun.incloud.match(regexp)[2]) {
+      // console.log(sampleRun.incloud.match(regexp));
+      incloudNumber = sampleRun.incloud.match(regexp)[0];
+    }
+
+    if (sampleRun.downloaded.match(regexp) && sampleRun.downloaded.match(regexp)[1] == sampleRun.downloaded.match(regexp)[2]) {
+      // console.log(sampleRun.downloaded.match(regexp));
+      downloadedNumber = sampleRun.downloaded.match(regexp)[0];
+      // console.log(downloadedNumber);
+
+    }
+
+    if (sampleRun.jobsubmitted.match(regexp) && sampleRun.jobsubmitted.match(regexp)[1] == sampleRun.jobsubmitted.match(regexp)[2]) {
+      // console.log(sampleRun.jobsubmitted.match(regexp));
+      jobsubmittedNumber = sampleRun.jobsubmitted.match(regexp)[0];
+    }
+
+    if (sampleRun.jobcompleted.match(regexp) && sampleRun.jobcompleted.match(regexp)[1] == sampleRun.jobcompleted.match(regexp)[2]) {
+      // console.log(sampleRun.jobcompleted.match(regexp));
+      jobcompletedNumber = sampleRun.jobcompleted.match(regexp)[0];
+    }
+
+    statusDetail.push({
+      "productType": productType[productTypeIndex],
+      "index": index,
+      "incloud": incloudNumber,
+      "downloaded": downloadedNumber,
+      "jobsubmitted": jobsubmittedNumber,
+      "jobcompleted": jobcompletedNumber
+    });
+  });
+
+  // console.log(statusDetail);
+  return statusDetail;
 }
 
 $(document).ready(function () {
@@ -151,8 +212,9 @@ $(document).ready(function () {
     '../SG/sg_progress_tail.json',
     '../IONA/iona_progress_tail.json',
   ];
+  let statusDetail = JSONListPath.forEach(getJSONList);
+  // console.log(statusDetail);
 
-  let progressJson = JSONListPath.forEach(getJSONList);
-  clickProgressIcon();
+  // clickProgressIcon();
 
 });
